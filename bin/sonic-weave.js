3975d381e11050f1bdd37b303300fb2d7871e90e
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-04-17 16:30:11 +0300

    Fix how the REPL is engaged
    ref #139

--- bin/sonic-weave.js
+++ bin/sonic-weave.js
@@ -4,16 +4,33 @@
 
 const {readFileSync} = require('fs');
 const {start} = require('repl');
+const {program} = require('commander');
+const {version} = require('../package.json');
 const {toScalaScl, repl} = require('../dist');
 
+program
+  .name('sonic-weave')
+  .description(
+    'CLI for the SonicWeave DSL for manipulating musical frequencies, ratios and equal temperaments'
+  )
+  .version(version)
+  .argument(
+    '[file]',
+    'File containing source code for a musical scale written in SonicWeave'
+  )
+  .option('-f, --format <format>', 'output format', 'scl');
+
 if (require.main === module) {
-  if (process.argv.length < 3) {
-    throw new Error('No input files given');
-  }
-  if (process.argv[2] === 'repl') {
+  program.parse();
+  const options = program.opts();
+  if (!program.args.length) {
     repl(start);
   } else {
-    const data = readFileSync(process.argv[2]);
-    process.stdout.write(toScalaScl(data.toString()));
+    const data = readFileSync(program.args[0]);
+    if (options.format === 'scl') {
+      process.stdout.write(toScalaScl(data.toString()));
+    } else {
+      process.stderr.write(`Unrecognized output format ${options.format}\n`);
+    }
   }
 }
