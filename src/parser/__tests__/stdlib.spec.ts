b5cceb8f4ea9f56ba78cb3080ca3711ff3b0b07f
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-05-14 19:05:41 +0300

    Update moment-of-symmetry dependency
    Fixes swapped MOS steps in large scales.

--- src/parser/__tests__/stdlib.spec.ts
+++ src/parser/__tests__/stdlib.spec.ts
@@ -1492,4 +1492,24 @@ describe('SonicWeave standard library', () => {
     const range = expand('range(3, 0, -1)');
     expect(range).toEqual(['3', '2', '1']);
   });
+
+  it('produces the correct 5L 8s scale', () => {
+    const scale = expand('mos(5, 8);stepString()');
+    expect(scale).toEqual([
+      '"LsLssLsLssLss"',
+      '2\\18',
+      '3\\18',
+      '5\\18',
+      '6\\18',
+      '7\\18',
+      '9\\18',
+      '10\\18',
+      '12\\18',
+      '13\\18',
+      '14\\18',
+      '16\\18',
+      '17\\18',
+      '18\\18',
+    ]);
+  });
 });
