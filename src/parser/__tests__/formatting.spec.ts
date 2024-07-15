b56dd8248cbf11b63723ef6a866c240aa4411083
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-07-15 19:52:34 +0300

    Implement best effort short string formatting
    ref #404

--- src/parser/__tests__/formatting.spec.ts
+++ src/parser/__tests__/formatting.spec.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest';
-import {getSourceVisitor, parseAST} from '..';
+import {evaluateExpression, getSourceVisitor, parseAST} from '..';
 import {repr} from '../../stdlib';
 
 function evaluate(source: string) {
@@ -69,5 +69,64 @@ describe('SonicWeave formatting semantics', () => {
   it('formats nedo unison', () => {
     const zeroOfFive = evaluate('5\\5 ~^ 0');
     expect(zeroOfFive).toBe('0\\5');
+  });
+});
+
+describe('Short label formatting', () => {
+  it('leaves short FJS alone', () => {
+    const min7 = evaluateExpression('lstr(min7, 9)');
+    expect(min7).toBe('min7');
+  });
+
+  it('turns long FJS into cents', () => {
+    const turboAugmented = evaluateExpression('lstr(Aug1 * 20, 9)');
+    expect(turboAugmented).toBe('2273.7001');
+  });
+
+  it('leaves short absoluteFJS alone', () => {
+    const Fsharp = evaluateExpression('C4 = 263Hz;lstr(F#4, 9)');
+    expect(Fsharp).toBe('F#4');
+  });
+
+  it('tries its best with long absoluteFJS', () => {
+    const cursed = evaluateExpression('C4 = 263z;lstr(C4 + Aug1 * 20, 9)');
+    expect(cursed).toBe('[1 1 -220 140>@Hz.263.2..');
+  });
+
+  it('turns long Hz into shorter Hz', () => {
+    const freq = evaluateExpression(
+      'C4 = 263z;lstr(linear(C4 + Aug1 * 20), 9)'
+    );
+    expect(freq).toBe('977.985Hz');
+  });
+
+  it('serves PI', () => {
+    const pie = evaluateExpression('lstr(PI, 9)');
+    expect(pie).toBe('3.141593r');
+  });
+
+  it('serves PI seconds', () => {
+    const pies = evaluateExpression('lstr(PI * 1s, 9)');
+    expect(pies).toBe('3.14159rs');
+  });
+
+  it('gives up on silly time exponents', () => {
+    const eeh = evaluateExpression('lstr(E * 1s^1.23456789e, 9)');
+    expect(eeh).toBe('2.718rs^*');
+  });
+
+  it('works with small values (linear)', () => {
+    const smol = evaluateExpression('lstr(-0.000000000000000123456789e, 9)');
+    expect(smol).toBe('-1.23e-16');
+  });
+
+  it('works with small values (logarithmic)', () => {
+    const smol = evaluateExpression('lstr(-0.000000123456, 9)');
+    expect(smol).toBe('-1.23e-7¢');
+  });
+
+  it('vectorizes', () => {
+    const three = evaluateExpression('lstr([3/2/^2, nan, -inf * 1z], 9)');
+    expect(three).toEqual(['3/2^1/2', 'nan', '-inf * 1Hz']);
   });
 });
